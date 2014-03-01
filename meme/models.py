20c649cb29e46dbe89c0b118fda11ed79066f550
samir <bhowmik.samir.r@gmail.com>
2014-03-01 15:36:42 +0200

    Memi

---
+++ meme/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Poll(models.Model):
+# ...
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.question
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+# ...
+    def __unicode__(self):  # Python 3: def __str__(self):
+        return self.choice_text
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
